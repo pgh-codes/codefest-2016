283d87592b1882e9b9451f0634b88e3e303ee0d6
Amy DePalma <amy@2206design.com>
2016-04-07 21:51:54 -0400

    Create Auto Route Flow
    & Added map to all pages
    & removed A tags around buttons
    & added preventDefault to all JS click events to prevent the form from
    submitting

--- www/assets/js/modal.js
+++ www/assets/js/modal.js
@@ -5,6 +5,10 @@ function openModal(modalId) {
     $(".modal-overlay").fadeTo(500, 0.7);
     $(".modal-container").fadeIn(500);
     
+    $("button.overlay").css({
+        'position' : 'absolute',
+        'z-index': '999'
+    });
 
     $(".modal-close, .modal-overlay").click(function () {
         $(".modal-container, .modal-overlay").fadeOut(500, function () {
--- www/assets/js/slideScreens.js
+++ www/assets/js/slideScreens.js
@@ -1,48 +1,74 @@
-$(".view-team-btn").click(function () {
-    if($("#export-data").hasClass("open-slide")) {
+// Full Screens
+
+$(".view-team-btn").click(function (event) {
+    event.preventDefault();
+    if ($("#export-data").hasClass("open-slide")) {
         $("#export-data").toggleClass("open-slide");
     }
-    if($("#add-team").hasClass("open-slide")) {
+    if ($("#add-team").hasClass("open-slide")) {
         $("#add-team").toggleClass("open-slide");
     }
-    $("#view-team").toggleClass("open-slide");  
+    $("#view-team").toggleClass("open-slide");
 });
 
 
-$(".add-team-btn").click(function () {
-    if($("#view-team").hasClass("open-slide")) {
+$(".add-team-btn").click(function (event) {
+    event.preventDefault();
+    if ($("#view-team").hasClass("open-slide")) {
         $("#view-team").toggleClass("open-slide");
-    }    
-    if($("#export-data").hasClass("open-slide")) {
+    }
+    if ($("#export-data").hasClass("open-slide")) {
         $("#export-data").toggleClass("open-slide");
-    }    
-    $("#add-team").toggleClass("open-slide");     
+    }
+    $("#add-team").toggleClass("open-slide");
 });
 
-
-$(".export-data-btn").click(function () {    
-    if($("#view-team").hasClass("open-slide")) {
+$(".export-data-btn").click(function (event) {
+    event.preventDefault();
+    if ($("#view-team").hasClass("open-slide")) {
         $("#view-team").toggleClass("open-slide");
-    }    
-    if($("#add-team").hasClass("open-slide")) {
+    }
+    if ($("#add-team").hasClass("open-slide")) {
         $("#add-team").toggleClass("open-slide");
-    }    
+    }
     $("#export-data").toggleClass("open-slide");
 });
 
-
-$(".close").click(function () {
+$(".close").click(function (event) {
+    event.preventDefault();
     $(this).parent().parent().toggleClass("open-slide");
 });
 
-
-$(".team-info-btn").click(function () {    
-   
+$(".team-info-btn").click(function (event) {
+    event.preventDefault();
     $("#team-info").toggleClass("open-slide");
 });
 
-$(".edit-member-btn").click(function () {    
-   
+$(".edit-member-btn").click(function (event) {
+    event.preventDefault();
     $("#edit-member").toggleClass("open-slide");
 });
 
+
+// Two Thirds Screens fo Directions
+function twoThirdsSlide(id) {
+    $(this).click(function (event) {
+        event.preventDefault();
+        $(id).toggleClass("open-partial");
+        $("button.overlay").toggleClass("hide");
+    });
+
+    $('#create-route').fadeOut(300, function () {
+        $('#create-route').toggleClass("open-slide");
+    });
+
+    $(".modal-overlay").css({
+        'display': 'none'
+    });
+}
+
+$(".close-partial").click(function (event) {
+    event.preventDefault();
+    $(this).parent().parent().toggleClass("open-partial");
+    $("button.overlay").toggleClass("hide");
+});
